92c30ed4cb8f4864a1919f01d4b38b63df4cd8d0
Mahmoud Ismail <ismailm7373@gmail.com>
2024-01-28 10:00:00 +0000

    Fizz-Buzz

---
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,26 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * main - draft
+ * Return: draft
+ */
+int main(void)
+{
+	int y;
+
+	for (y = 1; y < 101; y++)
+	{
+		if (y % 3 == 0 && y % 5 ==0)
+			printf("FizzBuzz");
+		else if (y % 3 == 0)
+			printf("Fizz");
+		else if (y % 5 == 0)
+			printf("Buzz");
+		else
+			printf("%d", y);
+		putchar(' ');
+	}
+	putchar('\n');
+	return (0);
+}
