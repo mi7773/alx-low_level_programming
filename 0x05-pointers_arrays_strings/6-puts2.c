3bd7ed153f929e1995d15476ef255064a264a468
Mahmoud Ismail <ismailm7373@gmail.com>
2024-02-03 11:18:03 +0000

    Half the lies they tell about me aren't true

--- 0x05-pointers_arrays_strings/6-puts2.c
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -9,8 +9,11 @@ void puts2(char *str)
 	while (*str != 0)
 	{
 		_putchar(*str);
-		str = str + 2;
 		if (*str == 0)
-			_putchar('\n');
+			str++;
+		else
+			str = str + 2;
 	}
+	_putchar('\n');
+
 }
