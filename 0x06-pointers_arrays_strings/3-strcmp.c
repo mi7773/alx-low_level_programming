c8a162151c4073e21554fc488ca80c68aab73d5f
Mahmoud Ismail <ismailm7373@gmail.com>
2024-02-04 06:13:03 +0000

    strcmp

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -8,16 +8,13 @@
  */
 int _strcmp(char *s1, char *s2)
 {
-	int c1 = 0, c2 = 0;
-
-	while (*s1++ != 0)
-		c1++;
-	while (*s2++ != 0)
-		c2++;
-	if (c1 == c2)
+	while (*s1 == *s2 && *s1 != 0)
+	{
+		s1++;
+		s2++;
+	}
+	if (*s1 == *s2)
 		return (0);
-	else if (c1 > c2)
-		return (15);
 	else
-		return (-15);
+		return (*s1 - *s2);
 }
