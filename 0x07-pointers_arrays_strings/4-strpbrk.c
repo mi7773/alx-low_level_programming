81311a8a8e02c47b377f44bb891bac18f1ad304e
Mahmoud Ismail <ismailm7373@gmail.com>
2024-02-05 03:57:03 +0000

    strpbrk

---
+++ 0x07-pointers_arrays_strings/4-strpbrk.c
@@ -0,0 +1,27 @@
+#include "main.h"
+
+/**
+ * _strpbrk - draft
+ * @s: draft
+ * @accept: draft
+ * Return: draft
+ */
+char *_strpbrk(char *s, char *accept)
+{
+	char *reset = accept;
+
+	while (*s != 0)
+	{
+		accept = reset;
+		while (*accept != 0)
+		{
+			if (*s == *accept)
+			{
+				return (s);
+			}
+			accept++;
+		}
+		s++;
+	}
+	return (s);
+}
