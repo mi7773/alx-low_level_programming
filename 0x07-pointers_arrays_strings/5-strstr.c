a5923b1a82f45dcc44b3c4174e3e1a0d70bd7488
Mahmoud Ismail <ismailm7373@gmail.com>
2024-02-05 17:27:34 +0000

    testing

---
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * _strstr - draft
+ * @haystack: draft
+ * @needle: draft
+ * Return: draft
+ */
+char *_strstr(char *haystack, char *needle)
+{
+	char *r;
+
+	while (*haystack != 0)
+	{
+		r = needle;
+		if (*haystack == *r)
+		{
+			while (*haystack == *r )
+			{
+				
+		s++;
+	}
