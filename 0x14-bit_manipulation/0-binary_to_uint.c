79ad6bd3b88f895309be5971d8f6e3c622e52391
Mahmoud Ismail <ismailm7373@gmail.com>
2024-02-08 10:22:21 +0000

    task

---
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -0,0 +1,32 @@
+#include "main.h"
+
+/**
+ * binary_to_uint - draft
+ * @b: draft
+ * Return: draft
+ */
+unsigned int binary_to_uint(const char *b)
+{
+	unsigned int r = 0;
+	int a = 1;
+	int c = 1;
+
+	if (b == 0)
+		return (0);
+	while (*++b != '\0')
+	{
+		c++;
+		a *= 2;
+	}
+	b = b - c;
+	while (*b != '\0')
+	{
+		if (*b == '0' || *b == '1')
+			r += (*b - 48) * a;
+		else
+			return (0);
+		b++;
+		a /= 2;
+	}
+	return (r);
+}
