c9a294f5bea3c7bf3bdd1291150389806b0cac48
Mahmoud Ismail <ismailm7373@gmail.com>
2024-02-08 15:30:36 +0000

    task

--- 0x14-bit_manipulation/1-print_binary.c
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -6,4 +6,22 @@
  */
 void print_binary(unsigned long int n)
 {
+	int o = 0, p;
+	unsigned long int m = n;
+
+	do {
+		m >>= 1;
+		o++;
+	} while (m);
+	p = o - 1;
+	while (o)
+	{
+		m = n;
+		if ((m >> p) & 1)
+			_putchar(49);
+		else if (((m >> p) & 1) == 0)
+			_putchar(48);
+		o--;
+		p--;
+	}
 }
