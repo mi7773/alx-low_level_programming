0af30027a3ada7de7fd9670d5efff48735f1fbaf
Mahmoud Ismail <ismailm7373@gmail.com>
2024-02-08 12:21:15 +0000

    task

---
+++ 0x14-bit_manipulation/4-clear_bit.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * clear_bit - draft
+ * @n: draft
+ * @index: draft
+ * Return: draft
+ */
+int clear_bit(unsigned long int *n, unsigned int index)
+{
+	int i = 1;
+
+	if (index > 32)
+		return (-1);
+	i <<= index;
+	if ((*n >> index) & 1)
+		*n ^= i;
+	else
+		*n &= i;
+	return (1);
+}
