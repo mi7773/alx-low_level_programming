045ddb74f676b5312fe58a8bf7edea583cdfde9e
Mahmoud Ismail <ismailm7373@gmail.com>
2024-02-08 15:38:19 +0000

    task

--- 0x14-bit_manipulation/4-clear_bit.c
+++ 0x14-bit_manipulation/4-clear_bit.c
@@ -8,14 +8,19 @@
  */
 int clear_bit(unsigned long int *n, unsigned int index)
 {
-	int i = 1;
+	int i = 1, o = 0;
+	unsigned long int m = n;
 
-	if (index > 32)
+	do {
+		o++;
+		m >>= 1;
+	} while (m);
+	if (index > 32 || index > o)
 		return (-1);
 	i <<= index;
 	if ((*n >> index) & 1)
 		*n ^= i;
 	else
 		*n &= i;
-	return (-1);
+	return (1);
 }
