758bd0c7f0fdad7f2303cc48957fd148acd5c6d4
Mahmoud Ismail <ismailm7373@gmail.com>
2024-02-10 15:39:55 +0000

    task

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -8,25 +8,29 @@
 unsigned int binary_to_uint(const char *b)
 {
 	unsigned int r = 0;
-	unsigned long a = 1;
+	int r0 = 1;
 	const char *z = b;
+	int c = 0;
+	int i;
 
-	if (b == 0)
-		return (0);
 	while (*b != '\0')
 	{
 		b++;
-		a *= 2;
 	}
-	b = z;
-	while (*b != '\0')
+	while (--b >= z)
 	{
-		if (*b == '0' || *b == '1')
-			r += (*b - 48) * (a / 2);
+		r0 = 1;
+		if (*b == 49)
+		{
+			for (i = 0; i < c; i++)
+				r0 *= 2;
+		}
+		else if (*b == 48)
+			r0 = 0;
 		else
 			return (0);
-		b++;
-		a /= 2;
+		r += r0;
+		c++;
 	}
 	return (r);
 }
