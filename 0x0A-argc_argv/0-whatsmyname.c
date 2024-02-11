66730c7057a5d84be5bb2ac03dfdaed0aa9e971b
Mahmoud Ismail <ismailm7373@gmail.com>
2024-02-11 12:33:52 +0000

    task

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/**
+ * main - draft
+ * @argc: draft
+ * @argv: drfat
+ * Return: draft
+ */
+int main(__attribute__((unused)) int argc, char **argv)
+{
+	printf("%s\n", argv[0]);
+	return (0);
+}
