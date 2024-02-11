10ff885191dda370fe89ac4e1f9de8e163487885
Mahmoud Ismail <ismailm7373@gmail.com>
2024-02-11 15:39:05 +0000

    task

---
+++ 0x0A-argc_argv/3-mul.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - draft
+ * @argc: draft
+ * @argv: draft
+ * Return: draft
+ */
+int main(int argc, char **argv)
+{
+	if (argc != 3)
+	{
+		printf("Error\n");
+		return (-1);
+	}
+	printf("%d\n", atoi(argv[1]) * atoi(*(argv + 2)));
+	return (0);
+}
