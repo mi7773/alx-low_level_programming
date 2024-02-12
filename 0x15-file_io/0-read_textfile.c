14a5ffadbbeea9cc65c4fc9859f0cb107d9ba4b8
Mahmoud Ismail <ismailm7373@gmail.com>
2024-02-12 17:28:01 +0000

    task

---
+++ 0x15-file_io/0-read_textfile.c
@@ -0,0 +1,33 @@
+#include <stddef.h>
+#include <unistd.h>
+#include <fcntl.h>
+#include <stdlib.h>
+#include "main.h"
+
+/**
+ * read_textfile - draft
+ * @filename: draft
+ * @letters: draft
+ * Return: draft
+ */
+ssize_t read_textfile(const char *filename, size_t letters)
+{
+	int fd;
+	ssize_t rdr, wrr;
+	char *c = (char *) malloc(sizeof(char) * ++letters);
+
+	if (filename == 0)
+		return (0);
+	fd = open(filename, O_RDONLY);
+	if (fd == -1)
+		return (0);
+	rdr = read(fd, c, --letters);
+	if (rdr == -1)
+		return (0);
+	c[rdr] = 0;
+	wrr = write(STDOUT_FILENO, c, rdr);
+	if (wrr == -1)
+		return (0);
+	free(c);
+	return (wrr);
+}
