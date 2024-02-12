8c96f9b60b26cad19d311cfd3d0796ad0cd1ed1f
Mahmoud Ismail <ismailm7373@gmail.com>
2024-02-12 18:53:00 +0000

    task

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -13,8 +13,8 @@
 ssize_t read_textfile(const char *filename, size_t letters)
 {
 	int fd;
-	ssize_t rdr, wrr, i;
-	char *c = (char *) malloc(sizeof(char) * letters), *p = c;
+	ssize_t rdr, wrr;
+	char *c = (char *) malloc(sizeof(char) * letters);
 
 	if (filename == 0)
 		return (0);
@@ -22,28 +22,12 @@ ssize_t read_textfile(const char *filename, size_t letters)
 	if (fd == -1)
 		return (0);
 	rdr = read(fd, c, letters);
-	switch (rdr)
-	{
-		case -1:
-			return (0);
-		case 0:
-			while (*p++ != 0)
-				i++;
-			wrr = write(STDOUT_FILENO, c, i);
-			break;
-		default:
-			wrr = write(STDOUT_FILENO, c, rdr);
-			break;
-	}
-	switch (wrr)
-	{
-		case -1:
-			return (0);
-		case 0:
-			return (i);
-		default:
-			return (wrr);
-	}
+	if (rdr == -1)
+		return (0);
+	wrr = write(STDOUT_FILENO, c, rdr);
+	if (wrr == -1)
+		return (0);
 	close(fd);
 	free(c);
+	return (wrr);
 }
