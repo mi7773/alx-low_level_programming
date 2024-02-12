250a13b7002cd66a9f131b04af792f18cbb6717a
Mahmoud Ismail <ismailm7373@gmail.com>
2024-02-12 21:49:39 +0000

    task

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -18,6 +18,8 @@ int create_file(const char *filename, char *text_content)
 	while (*text_content++ != 0)
 		i++;
 	fd = open(filename, O_TRUNC | O_CREAT | O_WRONLY, S_IRUSR | S_IWUSR);
+	if (fd == -1)
+		return (-1);
 	wrr = write(fd, text_content - i - 1, i);
 	if (wrr == -1)
 		return (-1);
