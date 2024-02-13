696be3912f858d01da868f1b7e8b67a1465f54c5
Mahmoud Ismail <ismailm7373@gmail.com>
2024-02-13 21:12:37 +0000

    task

---
+++ 0x0B-malloc_free/2-str_concat.c
@@ -0,0 +1,37 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * str_concat - draft
+ * @s1: draft
+ * @s2: draft
+ * Return: draft
+ */
+char *str_concat(char *s1, char *s2)
+{
+	int i = 0;
+	char *r, *p;
+
+	if (s1 == 0)
+		s1 = "";
+	if (s2 == 0)
+		s2 = "";
+	r = s1;
+	while (*r++ != 0)
+		i++;
+	r = s2;
+	while (*r++ != 0)
+		i++;
+	p = (char *) malloc(sizeof(char) * ++i);
+	if (p == 0)
+		return (0);
+	r = p;
+	while (*s1 != 0)
+		*r++ = *s1++;
+	--r;
+	while (*s2 != 0)
+		*r++ = *s2++;
+	*r = 0;
+	return (p);
+}
