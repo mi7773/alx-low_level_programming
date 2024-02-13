691bb2467755fbbc5a216ab239e3914d13f1cb39
Mahmoud Ismail <ismailm7373@gmail.com>
2024-02-13 09:44:33 +0000

    task

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -13,7 +13,7 @@
 int main(int ac, char **av)
 {
 
-	int fdf, fdt, cf, ct, rr;
+	int fdf, fdt, cf, ct, rr, wr;
 	char *c = (char *) malloc(1024 * sizeof(char));
 
 	if (ac != 3)
@@ -22,21 +22,21 @@ int main(int ac, char **av)
 		exit(97);
 	}
 	fdt = open(av[2], O_CREAT | O_TRUNC | O_WRONLY, 0664);
-	if (fdt == -1)
+	fdf = open(av[1], O_RDONLY);
+	do {
+		rr = read(fdf, c, 1024);
+		wr = write(fdt, c, rr);
+	} while (rr == 1024);
+	if (fdt == -1 || wr = -1)
 	{
 		dprintf(STDERR_FILENO, "Error: Can't write to %s\n", av[2]);
 		exit(99);
 	}
-	fdf = open(av[1], O_RDONLY);
-	if (fdf == -1)
+	if (fdf == -1 || rr == -1)
 	{
 		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", av[1]);
 		exit(98);
 	}
-	do {
-		rr = read(fdf, c, 1024);
-		write(fdt, c, rr);
-	} while (rr == 1024);
 	cf = close(fdf);
 	ct = close(fdt);
 	if (ct == -1)
