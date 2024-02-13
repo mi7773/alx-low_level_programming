7d268c105abd8856990ce184f0a21d62d0b5db8d
Mahmoud Ismail <ismailm7373@gmail.com>
2024-02-13 10:42:14 +0000

    task

--- 0x15-file_io/2-append_text_to_file.c
+++ 0x15-file_io/2-append_text_to_file.c
@@ -16,7 +16,7 @@ int append_text_to_file(const char *filename, char *text_content)
 	if (filename == 0)
 		return (-1);
 	if (text_content == 0)
-		text_content = "\n";
+		text_content = "\\n";
 	while (*text_content++ != 0)
 		i++;
 	fd = open(filename, O_WRONLY | O_APPEND);
