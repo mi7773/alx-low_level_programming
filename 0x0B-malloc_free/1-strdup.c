7c1e26e08d4f2e1f3d0f1239c2e9974bfc94012f
Mahmoud Ismail <ismailm7373@gmail.com>
2024-02-13 20:09:13 +0000

    task

---
+++ 0x0B-malloc_free/1-strdup.c
@@ -0,0 +1,25 @@
+#include <stdlib.h>
+#include "main.h"
+
+/**
+ * _strdup - draft
+ * @str: draft
+ * Return: draft
+ */
+char *_strdup(char *str)
+{
+	char *r = str;
+	int i = 0;
+
+	if (str == 0)
+		return (0);
+	while (*r++ != 0)
+		i++;
+	r = (char *) malloc(i * sizeof(char));
+	if (r == 0)
+		return (0);
+	while(*str != 0)
+		*r++ = *str++;
+	*r = 0;
+	return (r - i);
+}
