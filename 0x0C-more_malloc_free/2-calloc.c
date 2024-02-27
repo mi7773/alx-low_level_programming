9d9a7093a8453e5b2c307fa3e9791a9d7a28eb4d
Mahmoud Ismail <ismailm7373@gmail.com>
2024-02-27 00:33:58 +0000

    task

--- 0x0C-more_malloc_free/2-calloc.c
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -9,24 +9,44 @@
  */
 void *_calloc(unsigned int nmemb, unsigned int size)
 {
-	int *r;
 	unsigned int i;
 
 	if (nmemb == 0 || size == 0)
 	{
 		return (0);
 	}
-	r = malloc(nmemb * size);
-	if (r == 0)
+	if (size == sizeof(char))
 	{
-		return (0);
+		char *r = malloc(nmemb * size);
+		if (r == 0)
+		{
+			return (0);
+		}
+		i = 0;
+		while (i < nmemb)
+		{
+			r[i] = 0;
+			i++;
+		}
+
+		return (r);
 	}
-	i = 0;
-	while (i < nmemb)
+	if (size == sizeof(int))
 	{
-		r[i] = 0;
-		i++;
+		int *r = malloc(nmemb * size);
+		if (r == 0)
+		{
+			return (0);
+		}
+		i = 0;
+		while (i < nmemb)
+		{
+			r[i] = 0;
+			i++;
+		}
+
+		return (r);
 	}
 
-	return ((void *) r);
+	return (0);
 }
