ac17e644a29926748708039dfa60220888ddfcaa
Mahmoud Ismail <ismailm7373@gmail.com>
2024-03-08 12:17:02 +0000

    task

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -2,6 +2,21 @@
 #include "dog.h"
 
 /**
+ * copy - draft
+ * @s: drafr
+ * @d: draft
+ */
+void copy(char *s, char *d)
+{
+	int i = 0;
+
+	while (s[i])
+	{
+		d[i] = s[i];
+		i++;
+	}
+}
+/**
  * new_dog - draft
  * @name: draft
  * @age: draft
@@ -17,8 +32,18 @@ dog_t *new_dog(char *name, float age, char *owner)
 	{
 		return (0);
 	}
-	r->name = name;
-	r->age = age;
-	r->owner = owner;
+	if (name && owner)
+	{
+		r->name = (char *) malloc(sizeof(name));
+		copy(name, r->name);
+		r->age = age;
+		r->owner = (char *) malloc(sizeof(owner));
+		copy(owner, r->owner);
+	}
+	else
+	{
+		free (r);
+		return (0);
+	}
 	return (r);
 }
