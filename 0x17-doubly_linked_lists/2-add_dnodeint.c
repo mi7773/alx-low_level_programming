74d42f251645dba9a552abcc24696edd5a445b31
Mahmoud Ismail <ismailm7373@gmail.com>
2024-03-21 16:20:42 +0000

    task

---
+++ 0x17-doubly_linked_lists/2-add_dnodeint.c
@@ -0,0 +1,24 @@
+#include <stdlib.h>
+#include "lists.h"
+
+/**
+ * add_dnodeint - draft
+ * @head: draft
+ * @n: draft
+ *
+ * Return: draft
+ */
+dlistint_t *add_dnodeint(dlistint_t **head, const int n)
+{
+	dlistint_t *t = NULL;
+	t = malloc(sizeof(dlistint_t));
+	if (t == NULL)
+	{
+		return (NULL);
+	}
+	t->n = n;
+	t->prev = NULL;
+	t->next = *head;
+	*head = t;
+	return (*head);
+}
