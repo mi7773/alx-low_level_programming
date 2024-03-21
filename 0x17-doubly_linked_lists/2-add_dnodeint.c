0334939923ce9fd22e34fa7392b6f95cc8cb49c7
Mahmoud Ismail <ismailm7373@gmail.com>
2024-03-21 16:23:52 +0000

    task

--- 0x17-doubly_linked_lists/2-add_dnodeint.c
+++ 0x17-doubly_linked_lists/2-add_dnodeint.c
@@ -12,7 +12,7 @@ dlistint_t *add_dnodeint(dlistint_t **head, const int n)
 {
 	dlistint_t *t = NULL;
 
-	if !(head):
+	if (!head)
 		return (NULL);
 	t = malloc(sizeof(dlistint_t));
 	if (t == NULL)
