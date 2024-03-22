670cc10ba778842baf77063093e737a35e163a35
Mahmoud Ismail <ismailm7373@gmail.com>
2024-03-22 01:31:29 +0000

    task

---
+++ 0x17-doubly_linked_lists/3-add_dnodeint_end.c
@@ -0,0 +1,39 @@
+#include <stdlib.h>
+#include "lists.h"
+
+/**
+ * add_dnodeint - draft
+ * @head: draft
+ * @n: draft
+ *
+ * Return: draft
+ */
+dlistint_t *add_dnodeint_end(dlistint_t **head, const int n)
+{
+	dlistint_t *t = NULL;
+
+	t = malloc(sizeof(dlistint_t));
+	if (t == NULL)
+	{
+		return (NULL);
+	}
+	t->n = n;
+	t->next = NULL;
+	if (*head != NULL)
+	{
+		dlistint_t *p = *head;
+
+		while (p->next)
+		{
+			p = p->next;
+		}
+		t->prev = p;
+		p->next = t;
+	}
+	else
+	{
+		t->prev = NULL; 
+		*head = t;
+	}
+	return (t);
+}
